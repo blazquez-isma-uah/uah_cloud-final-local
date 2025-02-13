25af2eaeb3ddb154b1602411e52d93f3f7a78793
Ismael Blazquez Carabias <ismael.blazquez@edu.uah.es>
2025-02-13 23:10:43 +0100

    Versionado de password_generator

--- password_generator_backend/src/main/java/es/uah/ismael/fbm/passwordGeneratorBackend/controller/PasswordController.java
+++ password_generator_backend/src/main/java/es/uah/ismael/fbm/passwordGeneratorBackend/controller/PasswordController.java
@@ -1,11 +1,16 @@
 package es.uah.ismael.fbm.passwordGeneratorBackend.controller;
 
 import es.uah.ismael.fbm.passwordGeneratorBackend.utils.PasswordGenerator;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/password")
 public class PasswordController {
+    
+    @Value("${server.version}")
+    private String serverVersion;
 
     @GetMapping("/generate")
     public String generatePassword(
@@ -14,6 +19,6 @@ public class PasswordController {
             @RequestParam(defaultValue = "true") boolean useNumbers,
             @RequestParam(defaultValue = "true") boolean useSpecials) {
 
-        return PasswordGenerator.generatePassword(length, useUpper, useNumbers, useSpecials);
+        return serverVersion + "-" + PasswordGenerator.generatePassword(length, useUpper, useNumbers, useSpecials);
     }
 }
