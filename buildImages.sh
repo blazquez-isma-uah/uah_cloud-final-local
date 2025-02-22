08f03ea1ebc0445ae385bd0accc893ce40504fbf
Ismael Blazquez Carabias <ismael.blazquez@edu.uah.es>
2025-02-22 11:53:56 +0100

    Cambio de versiones

--- buildImages.sh
+++ buildImages.sh
@@ -1,11 +1,11 @@
 docker build -t ismabc4/peliculas_backend:0.1 ./backend_peliculas
 docker build -t ismabc4/usuarios_backend:0.1 ./backend_usuarios
-docker build -t ismabc4/password_generator_backend:0.2 ./password_generator_backend
+docker build -t ismabc4/password_generator_backend:0.1 ./password_generator_backend
 docker build -t ismabc4/frontend:0.1 ./frontend
 
 docker login
 
 docker push ismabc4/peliculas_backend:0.1
 docker push ismabc4/usuarios_backend:0.1
-docker push ismabc4/password_generator_backend:0.2
+docker push ismabc4/password_generator_backend:0.1
 docker push ismabc4/frontend:0.1
